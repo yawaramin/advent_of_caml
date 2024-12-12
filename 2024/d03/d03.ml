639448048695f57864bbbe296f394fdd93c5c6ff
Yawar Amin <yawar.amin@gmail.com>
2024-12-11 22:27:56 -0500

    2024 day 3 part 2

--- 2024/d03/d03.ml
+++ 2024/d03/d03.ml
@@ -13,5 +13,30 @@ let rec add_muls_to ?(pos = 0) sum line =
 
 let p1 () = Lib.fold_file_lines ~test __FILE__ 0 add_muls_to
 
-(* 159833790 *)
-let () = print_int (p1 ())
+let mul_do_dont =
+  Str.regexp
+    {|\(mul(\([0-9]?[0-9]?[0-9]?\),\([0-9]?[0-9]?[0-9]?\))\)\|\(do()\)\|\(don't()\)|}
+
+let rec add_muls_enabled ~pos (sum, enabled) line =
+  match Str.search_forward mul_do_dont line pos with
+  | pos -> (
+      let pos = succ pos in
+      match Str.matched_group 0 line with
+      | "do()" -> add_muls_enabled ~pos (sum, true) line
+      | "don't()" -> add_muls_enabled ~pos (sum, false) line
+      | _ ->
+          let product =
+            if enabled then
+              int_of_string (Str.matched_group 2 line)
+              * int_of_string (Str.matched_group 3 line)
+            else 0
+          in
+          add_muls_enabled ~pos (sum + product, enabled) line)
+  | exception Not_found -> (sum, enabled)
+
+let p2 () =
+  fst (Lib.fold_file_lines ~test __FILE__ (0, true) (add_muls_enabled ~pos:0))
+
+(* Part 1: 159833790
+   Part 2: 89349241 *)
+let () = Lib.print_parts p1 p2
